a8af990211124baacc27bdc3bb739d8533942480
ceorayyan <rayyan2405f@aptechorangi.com>
2026-03-18 08:54:31 +0500

    hm

--- app/Desktop/Sections/Banner.tsx
+++ app/Desktop/Sections/Banner.tsx
@@ -78,7 +78,7 @@ export default function Banner() {
 
   return (
     <section
-      className="relative w-full flex overflow-hidden "
+      className="relative w-full flex overflow-hidden mt-6"
       style={{ height: "70vh", minHeight: "30rem", maxHeight: "45rem" }}
     >
       {/* Background Image with fade + slide animation */}
