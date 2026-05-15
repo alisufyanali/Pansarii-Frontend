c8659345b25f06e2b925f38636e98539523a6bea
alisufyanali <alisufyan2410@gmail.com>
2026-05-16 00:12:27 +0500

    Double Provider Fix1

--- components/Mobile/layout.tsx
+++ components/Mobile/layout.tsx
@@ -1,9 +1,6 @@
-// app/Mobile/layout.tsx
 "use client";
 
 import { ReactNode, useState } from "react";
-import { CartProvider } from "@/context/CartContext";
-import { WishlistProvider } from "@/context/WishList";
 import Header from "./components/header";
 import MenuModal from "./components/MenuModal";
 import Footer from "./components/footer";
@@ -13,31 +10,24 @@ export default function MobileLayout({ children }: { children: ReactNode }) {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <CartProvider>
-      <WishlistProvider>
-        {/* Header - Fixed at top */}
-        <Header
-          isMenuOpen={isMenuOpen}
-          setIsMenuOpen={setIsMenuOpen}
-        />
+    <>
+      <Header
+        isMenuOpen={isMenuOpen}
+        setIsMenuOpen={setIsMenuOpen}
+      />
 
-        {/* Main Content */}
-        <main className="min-h-screen bg-white pt-32">
-          {children}
-        </main>
+      <main className="min-h-screen bg-white pt-32">
+        {children}
+      </main>
 
-        {/* Footer Section */}
-        <Footer />
+      <Footer />
 
-        {/* Bottom Nav */}
-        <BottomNav />
+      <BottomNav />
 
-        {/* Menu Modal */}
-        <MenuModal 
-          isOpen={isMenuOpen} 
-          onClose={() => setIsMenuOpen(false)} 
-        />
-      </WishlistProvider>
-    </CartProvider>
+      <MenuModal
+        isOpen={isMenuOpen}
+        onClose={() => setIsMenuOpen(false)}
+      />
+    </>
   );
 }
